6c5ca068a27849f85d1cebf6a7ac5b31b6160c8b
arijitchakma79 <arijitchakma79@gmail.com>
2025-10-27 01:44:37 -0400

    initial commit

---
+++ server.js
@@ -0,0 +1,16 @@
+let express = require("express");
+let app = express();
+let hostname = "localhost";
+let port = 3000;
+
+app.use(express.static('public'));
+
+
+app.get('/', (req, res) => {
+    console.log(req.originalUrl, req.headers, req.method);
+    res.sendFile(__dirname + '/public/index.html');
+  });
+
+app.listen(port, hostname, function () {
+  console.log(`http://${hostname}:${port}`);
+});
