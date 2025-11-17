95be0f313ce10401f06e89fdc4b267a5d085e43d
arijitchakma79 <arijitchakma79@gmail.com>
2025-11-17 15:39:53 -0500

    modified organization ui

--- server/routes/organizations.js
+++ server/routes/organizations.js
@@ -2,7 +2,12 @@ const express = require("express");
 const path = require("path");
 const router = express.Router();
 
-// GET /organizations/:id - Serve organization page
+// GET /organizations - Serve organizations list page
+router.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', '..', 'client', 'organizations', 'view.html'));
+});
+
+// GET /organizations/:id - Serve single organization page
 router.get('/:id', (req, res) => {
   res.sendFile(path.join(__dirname, '..', '..', 'client', 'organizations', 'view.html'));
 });
